e7bde16d03b825c33a4902b8544ae49f02490d22
MariaLoretto <maria_santos83@estudante.sesisenai.org.br>
2026-04-27 11:29:47 -0300

    add

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 let cadastro = false
 
 document.getElementById("toggle").onclick = () => {
+    cadastro = !cadastro;
+    document.getElementById("titulo").innerText = cadastro ? "Cadastro" : "Login";
+    document.querySelector("botton").innerText = cadastro ? "Cadastro" : "Entrar";
+    document.getElementById("toggle").innerText = cadastro
 
-
-    
 }
