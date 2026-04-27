ead8aa25522b10d556ab947db2302d9635477b97
MariaLoretto <maria_santos83@estudante.sesisenai.org.br>
2026-04-27 12:06:54 -0300

    add

--- scripts.js
+++ scripts.js
@@ -9,31 +9,39 @@ document.getElementById("toggle").onclick = () => {
     ? "Faça login"
     : "Não tem conta? Cadastre-se!";
     document.getElementById("mensagem").innerHTML = "";
-    document.getElementById("form-login").onsubmit= (e) => {
+}
+document.getElementById("form-login").onsubmit= (e) => {
     e.preventDefault();
+    
     let email = document.getElementById("email").value;
     let senha document.getElementById("senha").value;
     let mensagem document.getElementById("mensagem");
+
     mensagem.innerHTML = "";
+    
     if (!email.includes("@") || !email.includes(".")) {
     mensagem.innerHTML = "<div class='erro'><p> Email Inválido!</p></div>";
     return;
+
     }
+
     if (senha.length < 4) {
     mensagem.innerHTML = "<div class='erro'><p> Senha muito curta!</p></div>";
     return;
+
     }
-    if (cadastro) {
+
+    if (cadas0ro) {
     localStorage.setItem(email, senha);
     mensagem. innerHTML = "<div class='sucesso'><p> Cadastrado com sucesso!</p></div>";
     } else {
     let salva localStorage.getItem(email);
     if (salva senha) {
+
     mensagem.innerHTML = "<div class='sucesso'><p>Login com sucesso!</p></div>";
     } else {
     mensagem. innerHTML = "<div class='erro'><p>Dados Incorretos!</p></div>";
     }
     }
     document.getElementById("form-login").reset();
-    }
     }
